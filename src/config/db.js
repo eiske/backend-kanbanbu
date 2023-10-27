06cd0cad5ea242894c62c180aa50e407cbd053ff
Eiske <hashimoto.eisk@gmail.com>
2023-10-26 23:02:44 -0300

    Updated pg Pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-    user: "postgres",
-    host: process.env.APP_HOST,
-    database: "railway",
-    password: process.env.APP_PASSWORD,
-    port: process.env.PORT,
+    // user: "postgres",
+    // host: process.env.APP_HOST,
+    // database: "railway",
+    // password: process.env.APP_PASSWORD,
+    // port: process.env.PORT,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
